63af19ae041fbc071377280c0e4e8d7cb4da9524
SuperTikuwa <tikuwamk2@gmail.com>
2021-07-27 17:30:24 +0900

    fix: step6

--- src/step6/main_test.go
+++ src/step6/main_test.go
@@ -1,13 +1,14 @@
-// Step5
-// Student構造体を定義して，そのメンバーを表示するプログラムを制作してください．
+// Step6
+// student構造体を定義して，そのメンバーを表示するプログラムを制作してください．
 // 構造体のメンバ変数は，以下のように定義してください．(値は適当に決めてください)
-// 	Name: "Taro",
-// 	Age:  20,
-// 	Number: "K99999"
+// 	name: "Taro",
+// 	age:  20,
+// 	number: "K99999"
 package main
 
 import (
 	"regexp"
+	"strings"
 	"testing"
 
 	"github.com/SuperTikuwa/testutil"
@@ -15,9 +16,29 @@ import (
 
 func Test_main(t *testing.T) {
 	out := testutil.ExtractStdout(t, main)
-	reg := `{.+ [0-9]+ [a-zA-Z][0-9]{5}}`
 
-	if !regexp.MustCompile(reg).MatchString(out) {
-		t.Errorf("Wrong answer %s \n Match %s", out, reg)
+	if !strings.Contains(out, "name") || !strings.Contains(out, "age") || !strings.Contains(out, "number") {
+		t.Errorf("Wrong output\n")
 	}
+
+	taro := student{
+		name:   "Taro",
+		age:    20,
+		number: "K99999",
+	}
+
+	reg := `[a-zA-Z]+`
+	if !regexp.MustCompile(reg).MatchString(taro.name) {
+		t.Errorf("Wrong output\n")
+	}
+
+	if taro.age < 0 {
+		t.Errorf("Wrong output\n")
+	}
+
+	reg = `[a-zA-Z0-9]+`
+	if !regexp.MustCompile(reg).MatchString(taro.number) {
+		t.Errorf("Wrong output\n")
+	}
+
 }
